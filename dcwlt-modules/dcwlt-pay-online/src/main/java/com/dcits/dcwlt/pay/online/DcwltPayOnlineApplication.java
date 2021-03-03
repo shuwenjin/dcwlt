f4f6fd9803c32a30e494f04a5ee7657b3e69884b
yunghugh <yunghugh@gmail.com>
2021-03-03 19:44:07 +0800

    配置加载

--- dcwlt-modules/dcwlt-pay-online/src/main/java/com/dcits/dcwlt/pay/online/DcwltPayOnlineApplication.java
+++ dcwlt-modules/dcwlt-pay-online/src/main/java/com/dcits/dcwlt/pay/online/DcwltPayOnlineApplication.java
@@ -3,21 +3,25 @@ package com.dcits.dcwlt.pay.online;
  * 支付联机应用
  */
 
+import com.dcits.dcwlt.common.pay.util.ApplicationContextProvider;
 import com.dcits.dcwlt.common.security.annotation.EnableCustomConfig;
 import com.dcits.dcwlt.common.security.annotation.EnableRyFeignClients;
 import com.dcits.dcwlt.common.swagger.annotation.EnableCustomSwagger2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @EnableCustomConfig
 @EnableCustomSwagger2
 @EnableRyFeignClients
 @SpringCloudApplication
+@ImportResource("classpath:applicationContext.xml")
 public class DcwltPayOnlineApplication {
     public static void main(String[] args)
     {
-        SpringApplication.run(DcwltPayOnlineApplication.class, args);
+        ApplicationContextProvider.applicationContext = SpringApplication.run(DcwltPayOnlineApplication.class, args);
+
         System.out.println("#####  支付联机模块启动成功  #####\n");
     }
 }
