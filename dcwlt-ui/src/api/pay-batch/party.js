55a78a874cf14949cae08e4f015eb2ab54e40633
lihaochen888 <137214855@qq.com>
2021-03-05 18:09:23 +0800

    更新机构查询代码

--- dcwlt-ui/src/api/pay-batch/party.js
+++ dcwlt-ui/src/api/pay-batch/party.js
@@ -17,28 +17,3 @@ export function getParty(partyid) {
   })
 }
 
-// 新增机构
-export function addParty(data) {
-  return request({
-    url: '/pay-batch/party',
-    method: 'post',
-    data: data
-  })
-}
-
-// 修改机构
-export function updateParty(data) {
-  return request({
-    url: '/pay-batch/party',
-    method: 'put',
-    data: data
-  })
-}
-
-// 删除机构
-export function delParty(partyid) {
-  return request({
-    url: '/pay-batch/party/' + partyid,
-    method: 'delete'
-  })
-}
