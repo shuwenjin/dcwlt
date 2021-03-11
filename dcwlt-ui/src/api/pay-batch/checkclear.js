7e04a3900a209cdf1edfcd8a53b54cbb477ce771
dcr-yangjld <419216423@qq.com>
2021-03-11 15:57:11 +0800

    资金汇总核对查询

---
+++ dcwlt-ui/src/api/pay-batch/checkclear.js
@@ -0,0 +1,20 @@
+import request from '@/utils/request'
+
+// 查询资金调整汇总核对列表
+export function listCheckclear(query) {
+  return request({
+    url: '/pay-batch/checkclear/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询资金调整汇总核对详细
+export function getCheckclear(msgid) {
+  return request({
+    url: '/pay-batch/checkclear/' + msgid,
+    method: 'get'
+  })
+}
+
+}
