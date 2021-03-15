c40745912b6efbce300aa2050e58e2d0e730c3fa
dcr-yangjld <419216423@qq.com>
2021-03-15 15:27:18 +0800

    报表生成的实体

---
+++ dcwlt-api/dcwlt-api-pay/src/main/java/com/dcits/dcwlt/pay/api/model/FinanceReportDO.java
@@ -0,0 +1,164 @@
+package com.dcits.dcwlt.pay.api.model;
+
+import com.dcits.dcwlt.common.core.annotation.Excel;
+import com.dcits.dcwlt.common.core.web.domain.BaseEntity;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * 金融交易统计报表对象 pay_batch_finance_report
+ * 
+ * @author yangjld
+ * @date 2021-03-11
+ */
+public class FinanceReportDO extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 兑出金额 */
+    @Excel(name = "兑出金额")
+    private String cashoutAmount;
+
+    /** 兑出成功笔数 */
+    @Excel(name = "兑出成功笔数")
+    private String cashoutSuccessNumber;
+
+    /** 兑出总笔数 */
+    @Excel(name = "兑出总笔数")
+    private String cashoutTotalNumber;
+
+    /** 兑回金额 */
+    @Excel(name = "兑回金额")
+    private String cashinAmount;
+
+    /** 兑回成功笔数 */
+    @Excel(name = "兑回成功笔数")
+    private String cashinSuccessNumber;
+
+    /** 兑回总笔数 */
+    @Excel(name = "兑回总笔数")
+    private String cashinTotalNumber;
+
+    /** 汇款兑出金额 */
+    @Excel(name = "汇款兑出金额")
+    private String remitoutAmount;
+
+    /** 汇款兑出成功笔数 */
+    @Excel(name = "汇款兑出成功笔数")
+    private String remitoutSuccessNumber;
+
+    /** 汇款兑出总笔数 */
+    @Excel(name = "汇款兑出总笔数")
+    private String remitoutTotalNumber;
+
+    /** 报表日期 */
+    @Excel(name = "报表日期")
+    private String reportDate;
+
+    public void setCashoutAmount(String cashoutAmount) 
+    {
+        this.cashoutAmount = cashoutAmount;
+    }
+
+    public String getCashoutAmount() 
+    {
+        return cashoutAmount;
+    }
+    public void setCashoutSuccessNumber(String cashoutSuccessNumber) 
+    {
+        this.cashoutSuccessNumber = cashoutSuccessNumber;
+    }
+
+    public String getCashoutSuccessNumber() 
+    {
+        return cashoutSuccessNumber;
+    }
+    public void setCashoutTotalNumber(String cashoutTotalNumber) 
+    {
+        this.cashoutTotalNumber = cashoutTotalNumber;
+    }
+
+    public String getCashoutTotalNumber() 
+    {
+        return cashoutTotalNumber;
+    }
+    public void setCashinAmount(String cashinAmount) 
+    {
+        this.cashinAmount = cashinAmount;
+    }
+
+    public String getCashinAmount() 
+    {
+        return cashinAmount;
+    }
+    public void setCashinSuccessNumber(String cashinSuccessNumber) 
+    {
+        this.cashinSuccessNumber = cashinSuccessNumber;
+    }
+
+    public String getCashinSuccessNumber() 
+    {
+        return cashinSuccessNumber;
+    }
+    public void setCashinTotalNumber(String cashinTotalNumber) 
+    {
+        this.cashinTotalNumber = cashinTotalNumber;
+    }
+
+    public String getCashinTotalNumber() 
+    {
+        return cashinTotalNumber;
+    }
+    public void setRemitoutAmount(String remitoutAmount) 
+    {
+        this.remitoutAmount = remitoutAmount;
+    }
+
+    public String getRemitoutAmount() 
+    {
+        return remitoutAmount;
+    }
+    public void setRemitoutSuccessNumber(String remitoutSuccessNumber) 
+    {
+        this.remitoutSuccessNumber = remitoutSuccessNumber;
+    }
+
+    public String getRemitoutSuccessNumber() 
+    {
+        return remitoutSuccessNumber;
+    }
+    public void setRemitoutTotalNumber(String remitoutTotalNumber) 
+    {
+        this.remitoutTotalNumber = remitoutTotalNumber;
+    }
+
+    public String getRemitoutTotalNumber() 
+    {
+        return remitoutTotalNumber;
+    }
+    public void setReportDate(String reportDate) 
+    {
+        this.reportDate = reportDate;
+    }
+
+    public String getReportDate() 
+    {
+        return reportDate;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+            .append("cashoutAmount", getCashoutAmount())
+            .append("cashoutSuccessNumber", getCashoutSuccessNumber())
+            .append("cashoutTotalNumber", getCashoutTotalNumber())
+            .append("cashinAmount", getCashinAmount())
+            .append("cashinSuccessNumber", getCashinSuccessNumber())
+            .append("cashinTotalNumber", getCashinTotalNumber())
+            .append("remitoutAmount", getRemitoutAmount())
+            .append("remitoutSuccessNumber", getRemitoutSuccessNumber())
+            .append("remitoutTotalNumber", getRemitoutTotalNumber())
+            .append("reportDate", getReportDate())
+            .toString();
+    }
+}
---
+++ dcwlt-api/dcwlt-api-pay/src/main/java/com/dcits/dcwlt/pay/api/model/NonfinanceReportDO.java
@@ -0,0 +1,66 @@
+package com.dcits.dcwlt.pay.api.model;
+
+import com.dcits.dcwlt.common.core.annotation.Excel;
+import com.dcits.dcwlt.common.core.web.domain.BaseEntity;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * 非金融交易统计报表对象 pay_batch_nonfinance_report
+ * 
+ * @author yangjld
+ * @date 2021-03-11
+ */
+public class NonfinanceReportDO extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /** 绑卡数量 */
+    @Excel(name = "绑卡数量")
+    private String tiedCardNumber;
+
+    /** 解绑数量 */
+    @Excel(name = "解绑数量")
+    private String untieNumber;
+
+    /** 报表日期 */
+    @Excel(name = "报表日期")
+    private String reportDate;
+
+    public void setTiedCardNumber(String tiedCardNumber) 
+    {
+        this.tiedCardNumber = tiedCardNumber;
+    }
+
+    public String getTiedCardNumber() 
+    {
+        return tiedCardNumber;
+    }
+    public void setUntieNumber(String untieNumber) 
+    {
+        this.untieNumber = untieNumber;
+    }
+
+    public String getUntieNumber() 
+    {
+        return untieNumber;
+    }
+    public void setReportDate(String reportDate) 
+    {
+        this.reportDate = reportDate;
+    }
+
+    public String getReportDate() 
+    {
+        return reportDate;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+            .append("tiedCardNumber", getTiedCardNumber())
+            .append("untieNumber", getUntieNumber())
+            .append("reportDate", getReportDate())
+            .toString();
+    }
+}
