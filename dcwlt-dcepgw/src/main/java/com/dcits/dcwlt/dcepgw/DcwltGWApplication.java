0df748eb9f28aeb4e1e3aaadd07eb3dcff0e5219
AdamLatop <adamroberthall@sina.com>
2021-04-13 13:09:21 +0800

    报文转换功能添加

--- dcwlt-dcepgw/src/main/java/com/dcits/dcwlt/dcepgw/DcwltGWApplication.java
+++ dcwlt-dcepgw/src/main/java/com/dcits/dcwlt/dcepgw/DcwltGWApplication.java
@@ -4,12 +4,14 @@ import cn.hutool.crypto.asymmetric.SM2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 网关启动程序
  * 
  * @author yunghugh
  */
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class DcwltGWApplication
 {
