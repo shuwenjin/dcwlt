4dcb0a602c75e37c1f4f2fee21fb9c7aead9f1bc
yunghugh <yunghugh@gmail.com>
2021-04-14 15:04:16 +0800

    dcep网关报文拆组工具

--- dcwlt-dcepgw/src/main/java/com/dcits/dcwlt/dcepgw/DcwltGWApplication.java
+++ dcwlt-dcepgw/src/main/java/com/dcits/dcwlt/dcepgw/DcwltGWApplication.java
@@ -1,22 +1,17 @@
 package com.dcits.dcwlt.dcepgw;
 
-import cn.hutool.crypto.asymmetric.SM2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 网关启动程序
- * 
+ *
  * @author yunghugh
  */
-@EnableDiscoveryClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class DcwltGWApplication
-{
-    public static void main(String[] args)
-    {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class DcwltGWApplication {
+    public static void main(String[] args) {
         SpringApplication.run(DcwltGWApplication.class, args);
         System.out.println("#####  dcep网关启动成功  #####\n");
     }
