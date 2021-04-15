6ae8ebe8063195feafbc4ab8ea9d61c129fe7395
AdamLatop <adamroberthall@sina.com>
2021-04-15 13:12:15 +0800

    添加对账明细界面

---
+++ dcwlt-modules/dcwlt-pay-batch/src/main/java/com/dcits/dcwlt/pay/batch/controller/CheckPathDetailController.java
@@ -0,0 +1,38 @@
+package com.dcits.dcwlt.pay.batch.controller;
+
+import com.dcits.dcwlt.common.core.web.controller.BaseController;
+import com.dcits.dcwlt.common.core.web.page.TableDataInfo;
+import com.dcits.dcwlt.common.security.annotation.PreAuthorize;
+import com.dcits.dcwlt.pay.api.model.CheckPathDetialDO;
+import com.dcits.dcwlt.pay.batch.service.ICheckPathDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+/**
+ * 对账明细Controller
+ * 
+ * @author 
+ * @date 2021-03-09
+ */
+@RestController
+@RequestMapping("/checkpathdtl")
+public class CheckPathDetailController extends BaseController
+{
+    @Autowired
+    private ICheckPathDetailService checkPathDetailService;
+
+    /**
+     * 查询对账汇总列表
+     */
+    @PreAuthorize(hasPermi = "pay-batch:checkpath:list")
+    @GetMapping("/list")
+    public TableDataInfo list(String workdate, String batchId)
+    {
+        startPage();
+        List<CheckPathDetialDO> list = checkPathDetailService.select(workdate, batchId);
+        return getDataTable(list);
+    }
+}
