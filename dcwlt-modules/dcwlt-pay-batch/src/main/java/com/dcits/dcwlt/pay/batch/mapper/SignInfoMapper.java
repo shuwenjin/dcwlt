1db939af848e359ab57a325a44074a5a3db42da9
apple <1968895380@qq.com>
2021-04-20 19:36:50 +0800

    【dev】 【background】移除多余的 @author lihewei @date 2021-04-20 19：36

---
+++ dcwlt-modules/dcwlt-pay-batch/src/main/java/com/dcits/dcwlt/pay/batch/mapper/SignInfoMapper.java
@@ -0,0 +1,24 @@
+package com.dcits.dcwlt.pay.batch.mapper;
+
+import com.dcits.dcwlt.pay.api.model.SignInfoDO;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface SignInfoMapper {
+
+    int insert(SignInfoDO signInfoDO);
+
+    SignInfoDO selectByWltIdAndAcctId(SignInfoDO signInfoDO);
+
+    int updateBySignNo(SignInfoDO signInfoDO);
+
+    int updateByWltIdAndAcctId(SignInfoDO signInfoDO);
+
+    SignInfoDO selectBySignNo(String signNo);
+
+    List<SignInfoDO> selectPartSignInfo(SignInfoDO signInfoDO);
+}
+
+
---
+++ dcwlt-modules/dcwlt-pay-batch/src/main/java/com/dcits/dcwlt/pay/batch/service/ISignInfoService.java
@@ -0,0 +1,26 @@
+package com.dcits.dcwlt.pay.batch.service;
+
+import com.dcits.dcwlt.pay.api.model.SignInfoDO;
+
+import java.util.List;
+
+/**
+ * @author
+ * @date  2020/12/31
+ * @version 1.0.0
+ * <p>协议表DAO</p>
+ */
+public interface ISignInfoService {
+
+	SignInfoDO queryBySignNo(String signNo);
+
+	int insert(SignInfoDO signInfoDO);
+
+	int updateBySignNo(SignInfoDO signNo);
+
+	SignInfoDO selectByWltIdAndAcctId(SignInfoDO signInfoDO);
+
+	int updateByWltIdAndAcctId(SignInfoDO signInfoDO);
+
+	List<SignInfoDO> selectPartSignInfo(SignInfoDO signInfoDO);
+}
