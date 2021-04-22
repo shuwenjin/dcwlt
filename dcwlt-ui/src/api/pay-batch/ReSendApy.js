83b524922ba2937921215d32cf0465bb937a06be
apple <1968895380@qq.com>
2021-04-22 12:49:55 +0800

    优化了resend的url

--- dcwlt-ui/src/api/pay-batch/ReSendApy.js
+++ dcwlt-ui/src/api/pay-batch/ReSendApy.js
@@ -12,7 +12,7 @@ export function listReSendApy(query) {
 // 交易重发申请
 export function addReSendApy(data) {
   return request({
-    url: '/pay-online/ReSendApy/resend',
+    url: '/dcwlt-pay-online/ReSendApy/resend',
     method: 'post',
     data: data
   })
